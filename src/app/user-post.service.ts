3b9445ba26e083390b72a950955602d571521d19
ioanh <ioan.xristov@abv.bg>
2021-06-27 00:12:45 +0300

    creating the service

---
+++ src/app/user-post.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http'
+import { delay } from "rxjs/operators";
+import {UserItem} from './store/models/user.model'
+import {PostItem} from './store/models/post.model'
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserPostService {
+    constructor(private http: HttpClient) {}
+
+    getUsers(){
+        return this.http.get<Array<UserItem>>('http://jsonplaceholder.typicode.com/users')
+    }
+
+    getPost(id){
+        return this.http.get<PostItem>(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    }
+}
